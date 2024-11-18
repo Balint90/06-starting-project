8a09cc4f20201c39b49e3b679d8285cbf261c872
Bálint Csertán <csertanb@gmail.com>
2024-11-18 15:44:58 +0100

    with viewChild fn (can be undefined)

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,13 +11,14 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('newTicketForm') private newTicketForm?: ElementRef<HTMLFormElement>;
+  // @ViewChild('newTicketForm') private newTicketForm?: ElementRef<HTMLFormElement>;
+  private newTicketForm = viewChild<ElementRef<HTMLFormElement>>('newTicketForm');
 
   onSubmit(title: string, text: string) {
     console.log("Submitted!");
     console.log('Entered title ' + title);
     console.log("Text");
-    this.newTicketForm?.nativeElement.reset();
+    this.newTicketForm()?.nativeElement.reset();
   }
 
 }
